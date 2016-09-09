b2a65999e0c5c302ba03e9ac9dee0c0813482771
arpanrau <arpan.rau@gmail.com>
2016-09-09 10:50:12 -0400

    Updated my read LED sketch

--- sketch_read_LED/sketch_read_LED.ino
+++ sketch_read_LED/sketch_read_LED.ino
@@ -1,1 +1,21 @@
 
+int analogPin = 0;  //Infrared Sensor connected to analog pin 0
+
+int val_read = 0; // variable to store the value read
+
+void setup()
+
+{
+  Serial.begin(9600); //setup serial
+}
+
+void loop()
+   
+{
+   val_read = analogRead(analogPin); //read input pin
+   
+   Serial.println(val_read); //debug value
+   
+}
+
+
